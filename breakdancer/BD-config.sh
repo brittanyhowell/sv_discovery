97d480ce15b1d9f4e1aa933a762bf021c201585c
brittanyhowell <brittany.howell1@gmail.com>
2018-04-06 14:15:35 +0100

    Makes own file list Provide the directory and it will make a file list, then iterate over it.

--- breakdancer/BD-config.sh
+++ breakdancer/BD-config.sh
@@ -2,25 +2,37 @@
 
 # bsub -J "BDArray[1-4]" -o /nfs/team151/bh10/scripts/breakdancer_bh10/output/BD-%I-%J.out -e /nfs/team151/bh10/scripts/breakdancer_bh10/output/BD-%I-%J.err "/nfs/team151/bh10/scripts/breakdancer_bh10/runBD.sh /nfs/team151/bh10/scripts/breakdancer_bh10/fourBams.list"
 
-bamDIR=/lustre/scratch115/projects/interval_wgs/chr20bams/
+fileDIR=/lustre/scratch115/projects/interval_wgs/chr20bams/
+ext=".bam"
+
 swDIR=/nfs/users/nfs_k/kw8/software/breakdancer_20130808/breakdancer/
 oDIR=/lustre/scratch115/projects/interval_wgs/analysis/sv/breakdancer/config/
+listFiles=${oDIR}/fileList.txt
 
+## Create file with list of names
+touch ${listFiles}
+cd ${fileDIR}
+ls *${ext} > ${listFiles}
 
 # Get files with only names
-bamList=$1
-bamFiles=($(<"${bamList}"))
-BAM="${bamFiles[$((LSB_JOBINDEX-1))]}" 
+files=($(<"${listFiles}"))
+sFile="${files[$((LSB_JOBINDEX-1))]}" 
 
-# Get the absolute file location.
-bamLine=${bamDIR}/${BAM}
+echo "Working on: "${sFile}
 
-filename=${BAM%.bam}
+# # Get the absolute file location.
+fFile=${fileDIR}/${sFile}
+
+filename=${sFile%${ext}}
 
 oFile=${oDIR}/${filename}.config
 
 
     ## Run BD
-    echo "perl ${swDIR}/bam2cfg.pl -c 4 -q 20 -n 10000 {bamLine} > ${oFile}"
+    echo "perl ${swDIR}/bam2cfg.pl -c 4 -q 20 -n 10000 ${bamLine} > ${oFile}"
 
-    perl ${swDIR}/bam2cfg.pl -c 4 -q 20 -n 10000 {bamLine} > ${oFile}
+    perl ${swDIR}/bam2cfg.pl -c 4 -q 20 -n 10000 ${fFile} > ${oFile}
+
+rm ${listFiles}
+
+echo "completed!"
