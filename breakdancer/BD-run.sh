4c84880a73d82ff557108d73f1832bfc06964275
brittanyhowell <brittany.howell1@gmail.com>
2018-04-09 14:19:44 +0100

    run on small set of reHeadered bams

--- breakdancer/BD-run.sh
+++ breakdancer/BD-run.sh
@@ -1,32 +1,26 @@
 #!/bin/bash
 
-# bsub -J "BDArray[1-4]" -o /nfs/team151/bh10/scripts/breakdancer_bh10/output/BDHH-%I-%J.out -e /nfs/team151/bh10/scripts/breakdancer_bh10/output/BDHH-%I-%J.err "/nfs/team151/bh10/scripts/breakdancer_bh10/runBD.sh /lustre/scratch115/projects/interval_wgs/analysis/sv/breakdancer/config/config.list"
+# bsub -J "BDArray[1-226]" -o /nfs/team151/bh10/scripts/breakdancer_bh10/output/o_max_BD-%I-%J.out -e /nfs/team151/bh10/scripts/breakdancer_bh10/output/o_max_BD-%I-%J.err "/nfs/team151/bh10/scripts/breakdancer_bh10/BD-run.sh /lustre/scratch115/projects/interval_wgs/analysis/sv/breakdancer/WGbams_newheader/newBams.txt"
 
 configDIR=/lustre/scratch115/projects/interval_wgs/analysis/sv/breakdancer/config/
 swDIR=/nfs/users/nfs_k/kw8/software/breakdancer_20130808/breakdancer/
-oDIR=/lustre/scratch115/projects/interval_wgs/analysis/sv/breakdancer/real
+oDIR=/lustre/scratch115/projects/interval_wgs/analysis/sv/breakdancer/out/
 
 
 # Get files with only names
-bamList=$1
-bamFiles=($(<"${bamList}"))
-BAM="${bamFiles[$((LSB_JOBINDEX-1))]}" 
+configList=$1
+configFiles=($(<"${configList}"))
+config="${configFiles[$((LSB_JOBINDEX-1))]}" 
 
 # Get the absolute file location.
-bamLine=${configDIR}/${BAM}
+configLine=${configDIR}/${config}
 
-filename=${BAM%.config}
+filename=${config%.config}
 
-oFile=${oDIR}/${filename}.BD
+oFile=${oDIR}/${filename}.out
 
 
     ## Run BD
+	echo "${swDIR}/breakdancer-max -q 20 ${configLine} > ${oFile}"
 
-    ### Make config files 
-    # echo "perl ${swDIR}/bam2cfg.pl -c 4 -q 20 -n 10000 ${bamLine} > ${oFile}"
-
-    # perl ${swDIR}/bam2cfg.pl -c 4 -q 20 -n 10000 ${bamLine} > ${oFile}
-
-
-
-   ${swDIR}/breakdancer-max -q 20 ${bamLine} > ${oFile}
+   ${swDIR}/breakdancer-max -q 20 ${configLine} > ${oFile}
