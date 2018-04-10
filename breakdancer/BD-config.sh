8ab979d8ed245e522ce8fda31695c2e15f8d8b77
brittanyhowell <brittany.howell1@gmail.com>
2018-04-10 11:07:59 +0100

    Run with 226 WG bam samples

--- breakdancer/BD-config.sh
+++ breakdancer/BD-config.sh
@@ -1,20 +1,16 @@
 #!/bin/bash
 
-# bsub -J "BDArray[1-4]" -o /nfs/team151/bh10/scripts/breakdancer_bh10/output/BD-%I-%J.out -e /nfs/team151/bh10/scripts/breakdancer_bh10/output/BD-%I-%J.err "/nfs/team151/bh10/scripts/breakdancer_bh10/runBD.sh /nfs/team151/bh10/scripts/breakdancer_bh10/fourBams.list"
+# bsub -J "BDArray[1-226]" -o /nfs/team151/bh10/scripts/breakdancer_bh10/output/con_BD-%I-%J.out -e /nfs/team151/bh10/scripts/breakdancer_bh10/output/con_BD-%I-%J.err "/nfs/team151/bh10/scripts/breakdancer_bh10/config_BD.sh /nfs/team151/bh10/scripts/breakdancer_bh10/WGbams.list"
 
-fileDIR=/lustre/scratch115/projects/interval_wgs/chr20bams/
+fileDIR=/lustre/scratch115/projects/interval_wgs/WGbams
 ext=".bam"
 
 swDIR=/nfs/users/nfs_k/kw8/software/breakdancer_20130808/breakdancer/
 oDIR=/lustre/scratch115/projects/interval_wgs/analysis/sv/breakdancer/config/
-listFiles=${oDIR}/fileList.txt
 
-## Create file with list of names
-touch ${listFiles}
-cd ${fileDIR}
-ls *${ext} > ${listFiles}
 
 # Get files with only names
+listFiles=$1
 files=($(<"${listFiles}"))
 sFile="${files[$((LSB_JOBINDEX-1))]}" 
 
@@ -29,10 +25,9 @@ oFile=${oDIR}/${filename}.config
 
 
     ## Run BD
-    echo "perl ${swDIR}/bam2cfg.pl -c 4 -q 20 -n 10000 ${bamLine} > ${oFile}"
+    echo "perl swdir: ${swDIR}/bam2cfg.pl -c 4 -q 20 -n 10000 bamLine: ${bamLine} > oFile: ${oFile}"
 
     perl ${swDIR}/bam2cfg.pl -c 4 -q 20 -n 10000 ${fFile} > ${oFile}
 
-rm ${listFiles}
 
 echo "completed!"
