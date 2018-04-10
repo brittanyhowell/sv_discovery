b54c371112902d08c9968759f3b586da366a3ef1
brittanyhowell <brittany.howell1@gmail.com>
2018-04-10 11:21:08 +0100

    run over 226 WG bams

--- breakdancer/BD-run.sh
+++ breakdancer/BD-run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# bsub -J "BDArray[1-226]" -o /nfs/team151/bh10/scripts/breakdancer_bh10/output/o_max_BD-%I-%J.out -e /nfs/team151/bh10/scripts/breakdancer_bh10/output/o_max_BD-%I-%J.err "/nfs/team151/bh10/scripts/breakdancer_bh10/BD-run.sh /lustre/scratch115/projects/interval_wgs/analysis/sv/breakdancer/WGbams_newheader/newBams.txt"
+# bsub -J "BDArray[1-9]" -o /nfs/team151/bh10/scripts/breakdancer_bh10/output/o_max_BD-%I-%J.out -e /nfs/team151/bh10/scripts/breakdancer_bh10/output/o_max_BD-%I-%J.err "/nfs/team151/bh10/scripts/breakdancer_bh10/BD-run.sh /nfs/team151/bh10/scripts/breakdancer_bh10/WGbams_config.list"
 
 configDIR=/lustre/scratch115/projects/interval_wgs/analysis/sv/breakdancer/config/
 swDIR=/nfs/users/nfs_k/kw8/software/breakdancer_20130808/breakdancer/
@@ -21,6 +21,7 @@ oFile=${oDIR}/${filename}.out
 
 
     ## Run BD
+    echo "command"
 	echo "${swDIR}/breakdancer-max -q 20 ${configLine} > ${oFile}"
 
    ${swDIR}/breakdancer-max -q 20 ${configLine} > ${oFile}
