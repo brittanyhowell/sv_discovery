52956e154254ac81a9d73bb7b994360bacf8dc64
brittanyhowell <brittany.howell1@gmail.com>
2018-04-13 15:32:21 +0100

    added memory allowance

--- breakdancer/BD-run.sh
+++ breakdancer/BD-run.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-# bsub -J "BDArray[1-9]" -o /nfs/team151/bh10/scripts/breakdancer_bh10/output/o_max_BD-%I-%J.out -e /nfs/team151/bh10/scripts/breakdancer_bh10/output/o_max_BD-%I-%J.err "/nfs/team151/bh10/scripts/breakdancer_bh10/BD-run.sh /nfs/team151/bh10/scripts/breakdancer_bh10/WGbams_config.list"
+# bsub -J "BDmax[1-224]" -o /nfs/team151/bh10/scripts/breakdancer_bh10/output/BDmax/max_BD-%I-%J.out -e /nfs/team151/bh10/scripts/breakdancer_bh10/output/BDmax/max_BD-%I-%J.err "/nfs/team151/bh10/scripts/breakdancer_bh10/BD-run.sh /nfs/team151/bh10/scripts/breakdancer_bh10/WGbams_config.list"
 
-configDIR=/lustre/scratch115/projects/interval_wgs/analysis/sv/breakdancer/config/
+configDIR=/lustre/scratch115/projects/interval_wgs/analysis/sv/breakdancer/config
 swDIR=/nfs/users/nfs_k/kw8/software/breakdancer_20130808/breakdancer/
 oDIR=/lustre/scratch115/projects/interval_wgs/analysis/sv/breakdancer/out/
 
