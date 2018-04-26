620857f4878c6e336ac1494d5f6cfea8621848ea
brittanyhowell <brittany.howell1@gmail.com>
2018-04-26 12:21:39 +0100

    run for 3724 WG bams

--- breakdancer/BD-config.sh
+++ breakdancer/BD-config.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-# bsub -J "BDArray[1-226]" -o /nfs/team151/bh10/scripts/breakdancer_bh10/output/con_BD-%I-%J.out -e /nfs/team151/bh10/scripts/breakdancer_bh10/output/con_BD-%I-%J.err "/nfs/team151/bh10/scripts/breakdancer_bh10/config_BD_all.sh /nfs/team151/bh10/scripts/breakdancer_bh10/std_WGBams.list"
+# bsub -J "BDArray[1-3724]" -o /nfs/team151/bh10/scripts/breakdancer_bh10/output/con_BD-%I-%J.out -e /nfs/team151/bh10/scripts/breakdancer_bh10/output/con_BD-%I-%J.err "/nfs/team151/bh10/scripts/breakdancer_bh10/config_BD.sh /nfs/team151/bh10/scripts/bh10_general/fileLists/WG_bams.list"
 
-fileDIR=/lustre/scratch115/projects/interval_wgs/WGbams_std
+fileDIR=/lustre/scratch115/projects/interval_wgs/WGbams
 ext=".bam"
 
 swDIR=/nfs/users/nfs_k/kw8/software/breakdancer_20130808/breakdancer/
-oDIR=/lustre/scratch115/projects/interval_wgs/analysis/sv/breakdancer/config_std/
+oDIR=/lustre/scratch115/projects/interval_wgs/analysis/sv/breakdancer/config/config_WG_3724
 
 
 # Get files with only names
--- breakdancer/BD-run.sh
+++ breakdancer/BD-run.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-# bsub -J "BDmax[1-224]" -o /nfs/team151/bh10/scripts/breakdancer_bh10/output/BDmax/max_BD-%I-%J.out -e /nfs/team151/bh10/scripts/breakdancer_bh10/output/BDmax/max_BD-%I-%J.err "/nfs/team151/bh10/scripts/breakdancer_bh10/BD-run.sh /nfs/team151/bh10/scripts/breakdancer_bh10/WGbams_config.list"
+# bsub -J "BDmax[1-3724]" -o /nfs/team151/bh10/scripts/breakdancer_bh10/output/BDmax/max_BD-%I-%J.out -e /nfs/team151/bh10/scripts/breakdancer_bh10/output/BDmax/max_BD-%I-%J.err "/nfs/team151/bh10/scripts/breakdancer_bh10/BD-run.sh /nfs/team151/bh10/scripts/breakdancer_bh10/WGbams_config.list"
 
-configDIR=/lustre/scratch115/projects/interval_wgs/analysis/sv/breakdancer/config
+configDIR=/lustre/scratch115/projects/interval_wgs/analysis/sv/breakdancer/config/config_WG_3724
 swDIR=/nfs/users/nfs_k/kw8/software/breakdancer_20130808/breakdancer/
-oDIR=/lustre/scratch115/projects/interval_wgs/analysis/sv/breakdancer/out/
+oDIR=/lustre/scratch115/projects/interval_wgs/analysis/sv/breakdancer/out/WG_3724
 
 
 # Get files with only names
