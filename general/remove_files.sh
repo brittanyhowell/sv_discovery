a350c345bf587910eacf46ccc4b108aa0cdfad1b
brittanyhowell <brittany.howell1@gmail.com>
2018-04-29 17:02:40 +0100

    small easy script to remove a given list of files from any DIR

---
+++ general/remove_files.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# provide list of files, and DIR, and those files will be removed. 
+
+
+listFiles=/lustre/scratch115/projects/interval_wgs/WGbams/poor_quality_samples.list
+ext=".out"
+fileDIR=/lustre/scratch115/projects/interval_wgs/analysis/sv/breakdancer/out/WG_3724
+
+
+while read f; do
+  filename=$f$ext
+  echo "removing: ${filename}"
+  rm ${fileDIR}/$filename
+done <$listFiles
+
+echo "purge complete"
