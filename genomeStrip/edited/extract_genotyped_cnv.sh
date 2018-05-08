f0ce3c128fcf0e15035572d771ead4cb31f94690
brittanyhowell <brittany.howell1@gmail.com>
2018-05-08 14:23:42 +0100

    added relevant dir

--- genomeStrip/edited/extract_genotyped_cnv.sh
+++ genomeStrip/edited/extract_genotyped_cnv.sh
@@ -1,4 +1,7 @@
 ## Commands to extract table from full vcf
 
+
+# Output was in: 
+# /lustre/scratch115/projects/interval_wgs/analysis/sv/kw8/genomestrip/cnv_discovery/cnv_output/results
 ## Get columns chr, start, end, name, length and genotype per sample
 bcftools query -H -f'%CHROM\t%POS0\t%END\t%ID\t%GCLENGTH[\t%CN]\n'  gs_cnv.genotypes.vcf.gz > gs_cnv.reduced.genotypes.txt
