d8d606252307e72d64b52f905845bde503d457ce
brittanyhowell <brittany.howell1@gmail.com>
2018-05-15 20:22:47 +0100

    functional, now makes two output files. needs flags

--- general/intersect_genes.go
+++ general/intersect_genes.go
@@ -15,7 +15,7 @@ import (
 // 	end   int
 // )
 
-type hats struct {
+type dels struct {
 	chr   int
 	start int
 	end   int
@@ -28,8 +28,11 @@ type genes struct {
 	name  string
 }
 
-var g genes
-var d hats
+var (
+	g        genes
+	d        dels
+	delCount int
+)
 
 func main() {
 
@@ -70,6 +73,24 @@ func main() {
 	if err := sDel.Err(); err != nil {
 		log.Fatal(err)
 	}
+	outPath := "/Users/bh10/Documents/Rotation3/scripts/general/"
+	geneFullfile := "geneFullList.txt"
+
+	geneOut := fmt.Sprintf("%v%v", outPath, geneFullfile)
+	gOut, err := os.Create(geneOut)
+	if err != nil {
+		log.Fatalf("failed to create output %s: %v", geneOut, err)
+	}
+	defer gOut.Close()
+
+	genefile := "geneList.txt"
+
+	genesumOut := fmt.Sprintf("%v%v", outPath, genefile)
+	gsOut, err := os.Create(genesumOut)
+	if err != nil {
+		log.Fatalf("failed to create output %s: %v", genesumOut, err)
+	}
+	defer gsOut.Close()
 
 	// commence reading
 
@@ -93,7 +114,11 @@ func main() {
 			end:   gei,
 			name:  gname,
 		}
-		fmt.Println("this is the gene", g.chr, g.start, g.end, g.name)
+		fmt.Println()
+		fmt.Println("Gene for comparison:", g.chr, g.start, g.end, g.name)
+
+		// make count of dels 0, before searching dels for a match
+		delCount = 0
 
 		for _, rD := range delsAll {
 
@@ -107,16 +132,109 @@ func main() {
 			dsi := int(dsf)
 			dei := int(def)
 
-			d = hats{
+			d = dels{
 				chr:   dci,
 				start: dsi,
 				end:   dei,
 			}
+			fmt.Println("Current del: ", d.chr, d.start, d.end)
 
-			fmt.Println("3 from dels", d.chr, d.start, d.end)
+			// check chr the same
+			if g.chr != d.chr {
+				continue
+			}
 
+			if g.start <= d.end && g.end >= d.start {
+				delType := characteriseDel(g.start, g.end, d.start, d.end)
+				fmt.Println("Deletion found in gene, type: ", delType)
+
+				var p float32
+				switch delType {
+				case "within":
+					p = calcOverlap(d.end, d.start, g.end, g.start)
+				case "overspill":
+					p = 1
+				case "spillLeft":
+					p = calcLeftSpill(d.end, d.start, g.end, g.start)
+				case "spillRight":
+					p = calcRightSpill(d.end, d.start, g.end, g.start)
+				}
+				fmt.Println("p: ", p)
+
+				fmt.Fprintf(gOut, "%v\t%v\t%v\t%v\t%v\t%v\t%v\t%v\n",
+					g.name,
+					g.chr,
+					g.start,
+					g.end,
+					p,
+					d.chr,
+					d.start,
+					d.end,
+				)
+
+				fmt.Fprintf(gsOut, "%v\t%v\t%v\t%v\t%v\n",
+					g.name,
+					g.chr,
+					g.start,
+					g.end,
+					p,
+				)
+			}
+			// If there is a del
+			delCount++
+		}
+		// still print genes with no deletions for completeness
+		fmt.Println("no genes in ", g.name)
+		if delCount == 0 {
+			fmt.Fprintf(gsOut, "%v\t%v\t%v\t%v\t%v\n",
+				g.name,
+				g.chr,
+				g.start,
+				g.end,
+				0,
+			)
 		}
 	}
 }
 
-// fmt.Println("Dels:", dels)
+func characteriseDel(gstart, gend, dstart, dend int) string {
+	var delType string
+
+	switch {
+	case gstart <= dstart && gend >= dend:
+		delType = "within"
+	case gstart >= dstart && gend <= dend:
+		delType = "overspill"
+	case gstart >= dstart && gend >= dend:
+		delType = "spillLeft"
+	case gstart <= dstart && gend <= dend:
+		delType = "spillRight"
+	}
+	return delType
+
+}
+
+func calcRightSpill(dend, dstart, gend, gstart int) float32 {
+	oLen := g.end - d.start
+	genLen := g.end - g.start
+	var p float32
+	p = float32(oLen) / float32(genLen)
+	return p
+}
+
+func calcLeftSpill(dend, dstart, gend, gstart int) float32 {
+	oLen := d.end - g.start
+	genLen := g.end - g.start
+	var p float32
+	p = float32(oLen) / float32(genLen)
+	return p
+}
+
+func calcOverlap(dend, dstart, gend, gstart int) float32 {
+
+	delLen := d.end - d.start
+	genLen := g.end - g.start
+	var p float32
+	p = float32(delLen) / float32(genLen)
+	return p
+}
