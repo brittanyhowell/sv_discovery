6fefbb88e24a1378ae394b495a5ea5f507445653
brittanyhowell <brittany.howell1@gmail.com>
2018-05-16 11:24:37 +0100

    set up for array

--- general/run-intersect-genes.sh
+++ general/run-intersect-genes.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# bsub -J "int[1-3643]" -o /nfs/team151/bh10/scripts/bh10_general/output/intersectBD/intersect-%J-%I.out -e /nfs/team151/bh10/scripts/bh10_general/output/intersectBD/intersect-%J-%I.err "/nfs/team151/bh10/scripts/bh10_general/run-intersect-genes.sh 
+
+
 # outPath - output directory
 # geneOut - output file name
 # delIn - input deletions list
@@ -10,7 +13,11 @@
 scriptDIR=/nfs/team151/bh10/scripts/bh10_general/
 
 delInDIR=/lustre/scratch115/projects/interval_wgs/analysis/sv/breakdancer/filtered/WG_3642_XY/filtered/
-delIn="BD_filtered_DEL_EGAN00001362099.txt" 
+
+fileList="/nfs/team151/bh10/scripts/bh10_general/fileLists/BD-filtered.list"
+files=($(<"${fileList}"))
+delIn="${files[$((LSB_JOBINDEX-1))]}" 
+
 geneIn="/lustre/scratch115/projects/interval_wgs/analysis/gencode/gencode_GRCh38_exons_splice_sites_merged_v26/all_chr_protein_coding.txt"
 
 
@@ -25,7 +32,7 @@ geneOut="${sampleName}_geneList.txt"
 
 
 
-echo "commence intersect"
+echo "commence intersect for sample ${sampleName}"
 
 go run ${scriptDIR}/intersect_genes.go -outPath ${outPath} -geneOut ${geneOut} -delIn ${delInDIR}/${delIn} -geneIn ${geneIn}
 
