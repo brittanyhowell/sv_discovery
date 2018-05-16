69a3b56320f714acc0f345d58db4fd6b67c6a3b5
brittanyhowell <brittany.howell1@gmail.com>
2018-05-16 11:11:05 +0100

    pretty sure I like these ones, so I won't mess with details

--- general/run-intersect-genes.sh
+++ general/run-intersect-genes.sh
@@ -1,14 +1,33 @@
 #!/bin/bash
 
+# outPath - output directory
+# geneOut - output file name
+# delIn - input deletions list
+### Requirements: col1: chr, col2: start, col3: end,
+# geneIn - input gene list 	
+### Requirements: col1: chr, col2: start, col3: end, col4: name
 
-outPath="/Users/bh10/Documents/Rotation3/scripts/general/" 
-geneOut="geneList.txt" 
-delIn="/Users/bh10/Documents/Rotation3/scripts/general/test_dels.txt" 
-geneIn="/Users/bh10/Documents/Rotation3/scripts/general/test_genes.txt"
+scriptDIR=/nfs/team151/bh10/scripts/bh10_general/
+
+delInDIR=/lustre/scratch115/projects/interval_wgs/analysis/sv/breakdancer/filtered/WG_3642_XY/filtered/
+delIn="BD_filtered_DEL_EGAN00001362099.txt" 
+geneIn="/lustre/scratch115/projects/interval_wgs/analysis/gencode/gencode_GRCh38_exons_splice_sites_merged_v26/all_chr_protein_coding.txt"
+
+
+# Get sample name
+samplenoExt=${delIn%.txt}
+sampleName=$(echo $samplenoExt | sed 's/BD_filtered_DEL_//g')
+
+
+outPath="/lustre/scratch115/projects/interval_wgs/analysis/sv/geneIntersect/BD/" 
+geneOut="${sampleName}_geneList.txt" 
+
+
 
 
 echo "commence intersect"
 
-go run intersect_genes.go -outPath ${outPath} -geneOut ${geneOut} -delIn ${delIn} -geneIn ${geneIn}
+go run ${scriptDIR}/intersect_genes.go -outPath ${outPath} -geneOut ${geneOut} -delIn ${delInDIR}/${delIn} -geneIn ${geneIn}
 
 echo "complete"
+
