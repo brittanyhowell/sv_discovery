daf8faf87439dcb7d8cdaa7ccffef2ac50c08e84
brittanyhowell <brittany.howell1@gmail.com>
2018-05-18 13:21:18 +0100

    now extracts discovery sites

--- genomeStrip/edited/extract_genotyped_cnv.sh
+++ genomeStrip/edited/extract_genotyped_cnv.sh
@@ -2,6 +2,12 @@
 
 
 # Output was in: 
-# /lustre/scratch115/projects/interval_wgs/analysis/sv/kw8/genomestrip/cnv_discovery/cnv_output/results
+# /lustre/scratch115/projects/interval_wgs/analysis/sv/kw8/genomestrip/cnv_discovery/cnv_output/results/
 ## Get columns chr, start, end, name, length and genotype per sample
 bcftools query -H -f'%CHROM\t%POS0\t%END\t%ID\t%GCLENGTH[\t%CN]\n'  gs_cnv.genotypes.vcf.gz > gs_cnv.reduced.genotypes.txt
+
+
+## Not CNV, but disc:
+
+bcftools query -H -f'%CHROM\t%POS0\t%END\t%ID\t%FILTER\n'  gs_dels.sites.vcf > gs_dels.sites.reduced.txt
+
