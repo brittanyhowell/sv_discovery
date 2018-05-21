4a5a8db45e4cba4cf57e7b68ff3c574aed6a023f
brittanyhowell <brittany.howell1@gmail.com>
2018-05-21 16:52:13 +0100

    the hungry hungry script (merges dfs from intersect.go)

---
+++ general/merge_intersectGenes.R
@@ -0,0 +1,40 @@
+
+
+geneDIR <- "/lustre/scratch115/projects/interval_wgs/analysis/sv/geneIntersect/BD"
+# geneDIR <- "~/Documents/Rotation3/data/gene_intersect/"
+outTable <- "/lustre/scratch115/projects/interval_wgs/analysis/sv/geneIntersect/BD-merge/merge.txt"
+
+
+
+genefiles <- dir(geneDIR)
+
+
+# Open the file, strip the colums, bind it to the current one.
+totGenes <- NULL
+for( i in 1:length(genefiles) ){
+  
+  file <- genefiles[i]
+  nameFile <- gsub("_geneList.txt","",file)
+  
+  geneLoc <- paste(geneDIR,file, sep="/")
+  genetable <- read.table(geneLoc)
+  
+  colnames(genetable) <- c("Exon", nameFile)
+
+  
+  if (i==1) {
+    totGenes <- genetable
+  } else {
+    print(nameFile)
+    totGenes <- merge(totGenes, genetable, by.x=c("Exon"), by.y=c("Exon"))
+    
+
+    gc()
+    
+  }
+
+}
+print("writing table to")
+print(outTable)
+write.table(totGenes, outTable, quote=F, row.names=F,  sep="\t")
+
