8b00285e029d215a7f2b0313f4fbe244863792c4
brittanyhowell <brittany.howell1@gmail.com>
2018-05-24 10:35:28 +0100

    check recip

--- general/run-recip_intersect.sh
+++ general/run-recip_intersect.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-# bsub  -o /nfs/team151/bh10/scripts/bh10_general/output/intersects/intersect-%J.out -e /nfs/team151/bh10/scripts/bh10_general/output/intersects/intersect-%J.err -R"select[mem>1000] rusage[mem=1000]" -M1000 /nfs/team151/bh10/scripts/bh10_general/run-recip_intersect-GS-CNV.sh
+# bsub  -o /nfs/team151/bh10/scripts/bh10_general/output/intersects/intersect-%J.out -e /nfs/team151/bh10/scripts/bh10_general/output/intersects/intersect-%J.err -R"select[mem>1000] rusage[mem=1000]" -M1000 /nfs/team151/bh10/scripts/bh10_general/run-recip_intersect-CNV-GS.sh
 
 echo "commence"
 scriptDIR=/nfs/team151/bh10/scripts/bh10_general/
 
 
 
-fileA="/lustre/scratch115/projects/interval_wgs/analysis/sv/genomestrip/filtered_sv/GS_filtered_DEL_disc.txt" 
-fileA_type="GS_disc"
-fileB="/lustre/scratch115/projects/interval_wgs/analysis/sv/genomestrip/filtered_sv/GS_filtered_DEL_CNV_no_genotype_info.txt"
-fileB_type="CNV"
+fileA="/lustre/scratch115/projects/interval_wgs/analysis/sv/genomestrip/filtered_sv/GS_filtered_DEL_CNV_no_genotype_info.txt" 
+fileA_type="CNV"
+fileB="/lustre/scratch115/projects/interval_wgs/analysis/sv/genomestrip/filtered_sv/GS_filtered_DEL_disc.txt"
+fileB_type="GS_disc"
 outDIR="/lustre/scratch115/projects/interval_wgs/analysis/sv/intersects/"
 out="intersect_${fileA_type}_${fileB_type}.txt"
 
