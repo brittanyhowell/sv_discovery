8c940dde4ca2c217be1412cc295ad333d3efcdc3
brittanyhowell <brittany.howell1@gmail.com>
2018-05-24 10:11:10 +0100

    GS-disc--BD

--- general/run-recip_intersect.sh
+++ general/run-recip_intersect.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-# bsub  -o /nfs/team151/bh10/scripts/bh10_general/output/intersects/intersect-%J.out -e /nfs/team151/bh10/scripts/bh10_general/output/intersects/intersect-%J.err -R"select[mem>1000] rusage[mem=1000]" -M1000 /nfs/team151/bh10/scripts/bh10_general/run-recip_intersect.sh
+# bsub  -o /nfs/team151/bh10/scripts/bh10_general/output/intersects/intersect-%J.out -e /nfs/team151/bh10/scripts/bh10_general/output/intersects/intersect-%J.err -R"select[mem>1000] rusage[mem=1000]" -M1000 /nfs/team151/bh10/scripts/bh10_general/run-recip_intersect-GS-BD.sh
 
 echo "commence"
 scriptDIR=/nfs/team151/bh10/scripts/bh10_general/
 
 
 
-fileA="/lustre/scratch115/projects/interval_wgs/analysis/sv/genomestrip/filtered_sv/GS_filtered_DEL_CNV_no_genotype_info.txt" 
+fileA="/lustre/scratch115/projects/interval_wgs/analysis/sv/genomestrip/filtered_sv/GS_filtered_DEL_disc.txt" 
 fileA_type="GS_disc"
 fileB="/lustre/scratch115/projects/interval_wgs/analysis/sv/breakdancer/filtered/WG_3642_XY/BD_filtered_DEL_224_sorted.txt"
 fileB_type="BD"
