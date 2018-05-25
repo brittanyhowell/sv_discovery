e3e534198053be8bb4a73896dd023d34fc5c0cdb
brittanyhowell <brittany.howell1@gmail.com>
2018-05-25 10:32:56 +0100

    var change

--- general/run-filter_repeats.sh
+++ general/run-filter_repeats.sh
@@ -2,7 +2,7 @@
 
 # bsub -J "int[1-226]"  -o /nfs/team151/bh10/scripts/bh10_general/output/repeatFilter/repFilt-%J-%I.out -e /nfs/team151/bh10/scripts/bh10_general/output/repeatFilter/repFilt-%J-%I.err  -R"select[mem>1000] rusage[mem=1000]" -M1000  "/nfs/team151/bh10/scripts/bh10_general/run-filter_repeats.sh" 
 
-# bsub  -o /nfs/team151/bh10/scripts/bh10_general/output/repeatFilter/repFilt-%J-%I.out -e /nfs/team151/bh10/scripts/bh10_general/output/repeatFilter/repFilt-%J-%I.err  -R"select[mem>1000] rusage[mem=1000]" -M1000  "/nfs/team151/bh10/scripts/bh10_general/run-filter_repeats.sh" 
+# bsub  -o /nfs/team151/bh10/scripts/bh10_general/output/repeatFilter/repFilt-%J-%I.out -e /nfs/team151/bh10/scripts/bh10_general/output/repeatFilter/repFilt-%J-%I.err  -R"select[mem>3000] rusage[mem=3000]" -M3000  "/nfs/team151/bh10/scripts/bh10_general/run-filter_repeats-CNV.sh" 
 
 # outPath - output directory
 # filtOut - output file name
@@ -23,15 +23,16 @@ delInDIR=/lustre/scratch115/projects/interval_wgs/analysis/sv/genomestrip/filter
 # fileList="/nfs/team151/bh10/scripts/bh10_general/fileLists/GS-genotyped-SV-filtered.list"
 # files=($(<"${fileList}"))
 # delIn="${files[$((LSB_JOBINDEX-1))]}" 
-delIn="GS_filtered_DEL_disc.txt"
-
+# delIn="GS_filtered_DEL_disc.txt"
+delIn="GS_filtered_DEL_CNV.txt"
+# delIn="BD_filtered_DEL_disc.txt"
 
 repIn="/lustre/scratch115/projects/interval_wgs/analysis/gatk/Homo_sapiens_assembly38/ucsc_repeats_GRCh38.txt.gz"
 
 
 # Get sample name
 samplenoExt=${delIn%.txt}
-sampleName=$(echo $samplenoExt | sed 's/GS_filtered_DEL_//g')
+sampleName=$(echo $samplenoExt | sed 's/GS_filtered_DEL_//g'  | sed 's/BD_filtered_DEL_//g')
 
 
 outPath="/lustre/scratch115/projects/interval_wgs/analysis/sv/genomestrip/filtered_sv" 
