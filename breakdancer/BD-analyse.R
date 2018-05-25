2c24ad9219fd442b8994455b4c9b26b7135250eb
brittanyhowell <brittany.howell1@gmail.com>
2018-05-25 10:26:44 +0100

    moved most of this analysis in to cross-platform-comparison.R

--- breakdancer/BD-analyse.R
+++ breakdancer/BD-analyse.R
@@ -5,38 +5,28 @@ library(ggplot2)
 
 
 setwd("/Users/bh10/Documents/Rotation3/data/BD/filtered")
-df.sub <-  read.table("BD_filtered_DEL_3642_sorted_10000.txt", header = TRUE)
 
 
+df.226 <-  read.table("/Users/bh10/Documents/Rotation3/data/BD/filtered/BD_filtered_DEL_224_sorted.txt", header = TRUE)
+colnames(df.226) <- c("chr", "start", "end", "length", "c", "n","m","o","p" )
 
-ggplot(dfsub, aes(x=SizeSV, fill=Chr1)) +
-  geom_density(alpha=0.20) + 
-  scale_x_continuous(limits = c(0, 1000)) + 
-  theme_bw()
-
-
-df.all <-  read.table("BD_filtered_DEL_3642_sorted.txt", header = TRUE)
-colnames(df.all) <- c("chr", "start", "end", "length", "c", "n","m","o","p" )
-
-df.all$chr <-  as.character(df.all$chr)
-ggplot(df.all, aes(x=length, fill=chr))+
+df.226$chr <-  factor(df.226$chr, levels= c(1:24))
+ggplot(df.226, aes(x=length, fill=chr))+
   geom_density(alpha=0.40) + 
-  scale_x_continuous(limits = c( 3000,7000)) + 
-  theme_bw()
+  scale_x_continuous(limits = c( 000,8000)) + 
+  theme_bw() +
+  
 
 
-df.some <-  read.table("BD_filtered_DEL_3642_sorted_100000.txt", header = TRUE)
-colnames(df.some) <- c("chr", "start", "end", "length", "c", "n","m","o","p" )
-ggplot(df.some, aes(x=length, fill=chr)) +
-  geom_density(alpha=0.20) + 
-  scale_x_continuous(limits = c(0, 30000)) + 
-  theme_bw()
-
-
-ggplot(data=df.some, aes(x=Pos1, y=Pos2)) +
-  geom_line(x=x, y=1) + 
-  scale_x_continuous(limits = c(0, 30000)) + 
-  theme_bw()
+# df.all <-  read.table("BD_filtered_DEL_3642_sorted.txt", header = TRUE)
+# colnames(df.all) <- c("chr", "start", "end", "length", "c", "n","m","o","p" )
+# 
+# df.all$chr <-  factor(df.all$chr, levels= c(1:22,"X","Y","M"))
+# ggplot(df.all, aes(x=length, fill=chr))+
+#   geom_density(alpha=0.40) + 
+#   scale_x_continuous(limits = c( 500,8000)) + 
+#   theme_bw()
+# 
 
 
 library(IRanges)
