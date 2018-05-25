284c58059f27f0a0c1bf3063d994ae570b9e27a9
brittanyhowell <brittany.howell1@gmail.com>
2018-05-25 10:33:21 +0100

    Change the function of this script, really. I made it so it takes in several samples, and processes them all into a table with the number of unique intersecting intervals per sample. It's fundamentally changed the purpose of the script, and if I want to just intersect two regions and find the intersecting regions, I recommend leaving this script as it, and instead writing a new one.

--- general/run-recip_intersect.sh
+++ general/run-recip_intersect.sh
@@ -1,23 +1,76 @@
 #!/bin/bash
 
-# bsub  -o /nfs/team151/bh10/scripts/bh10_general/output/intersects/intersect-%J.out -e /nfs/team151/bh10/scripts/bh10_general/output/intersects/intersect-%J.err -R"select[mem>1000] rusage[mem=1000]" -M1000 /nfs/team151/bh10/scripts/bh10_general/run-recip_intersect-CNV-GS.sh
+# bsub  -o /nfs/team151/bh10/scripts/bh10_general/output/intersects/intersect-%J.out -e /nfs/team151/bh10/scripts/bh10_general/output/intersects/intersect-%J.err -R"select[mem>1000] rusage[mem=1000]" -M1000 /nfs/team151/bh10/scripts/bh10_general/run-recip_intersect_per_sample.sh
 
 echo "commence"
 scriptDIR=/nfs/team151/bh10/scripts/bh10_general/
 
 
-
-fileA="/lustre/scratch115/projects/interval_wgs/analysis/sv/genomestrip/filtered_sv/GS_filtered_DEL_CNV_no_genotype_info.txt" 
-fileA_type="CNV"
-fileB="/lustre/scratch115/projects/interval_wgs/analysis/sv/genomestrip/filtered_sv/GS_filtered_DEL_disc.txt"
-fileB_type="GS_disc"
-outDIR="/lustre/scratch115/projects/interval_wgs/analysis/sv/intersects/"
-out="intersect_${fileA_type}_${fileB_type}.txt"
+sampleList=/nfs/team151/bh10/scripts/bh10_general/fileLists/226Samples.list
 
 
-echo "run intersect for ${fileA_type} and ${fileB_type}"
+BDDir="/lustre/scratch115/projects/interval_wgs/analysis/sv/breakdancer/filtered/WG_3642_XY/226subset" 
+GSDir="/lustre/scratch115/projects/interval_wgs/analysis/sv/genomestrip/filtered_sv/split_tables_CNV_genotyped"
 
-echo "command:  go run ${scriptDIR}/recip_intersect.go -oneIn=${fileA} -twoIn=${fileB} -outPath=${outDIR} -out=${out}"
-go run ${scriptDIR}/recip_intersect.go -oneIn=${fileA} -twoIn=${fileB} -outPath=${outDIR} -out=${out}
+outDIR="/lustre/scratch115/projects/interval_wgs/analysis/sv/intersects/sampleIntersect/"
+sumOut="${outDIR}/sum_intersect_BD_CNV.txt"
+rm ${sumOut}
+touch ${sumOut}
+
+printf '%s\t%s\t%s\t%s\t%s\t%s\n' "sample" "uniqBD" "uniqGS" "intersect" "rawBD" "rawGS">> ${sumOut}
+
+
+echo "run intersect for each sample"
+
+
+while read p; do
+
+	sampleBD="BD_filtered_DEL_${p}"
+	sampleGS="GS_filtered_DEL_CNV_${p}"
+
+	fileBD=${BDDir}/${sampleBD}
+	fileGS=${GSDir}/${sampleGS}
+
+	linesBD=$(cat ${fileBD}|wc -l )
+	linesGS=$(cat ${fileGS}|wc -l )
+
+	if [[ -f ${fileBD} && ${fileGS} ]]; then
+		
+		out="intersect_${p}"
+
+		echo "command:  go run ${scriptDIR}/recip_intersect.go -oneIn=${fileBD} -twoIn=${fileGS} -outPath=${outDIR} -out=${out}"
+		go run ${scriptDIR}/recip_intersect.go -oneIn=${fileBD} -twoIn=${fileGS} -outPath=${outDIR} -out=${out}
+
+		
+		cd ${outDIR}
+		uniqA=$(cat ${out}| awk '{print $1"-"$2":"$3}' | sort | uniq | wc -l )
+		uniqB=$(cat ${out}| awk '{print $5"-"$6":"$7}' | sort | uniq | wc -l )
+		all=$(cat ${out} | wc -l)
+
+
+
+		
+		printf '%s\t%s\t%s\t%s\t%s\t%s\n' "$p" "${uniqA}" "${uniqB}" "${all}" "${linesBD}" "${linesGS}">> ${sumOut}
+
+	else
+	    echo "one of your files did not exist, it has been skipped."
+	fi
+
+
+
+done <${sampleList}
+
 
 echo "complete"
+
+
+
+#"/lustre/scratch115/projects/interval_wgs/analysis/sv/genomestrip/filtered_sv/GS_filtered_DEL_disc.txt"
+#"/lustre/scratch115/projects/interval_wgs/analysis/sv/genomestrip/filtered_sv/GS_filtered_DEL_CNV_no_genotype_info.txt"
+#"/lustre/scratch115/projects/interval_wgs/analysis/sv/breakdancer/filtered/WG_3642_XY/BD_filtered_DEL_224_sorted.txt"
+
+
+# /lustre/scratch115/projects/interval_wgs/analysis/sv/intersects/intersect_GS_disc_CNV.txt
+
+
+
